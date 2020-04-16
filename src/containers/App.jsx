3009db6dff607c9c55ad4bc21d5440be566b1a81
Alvaro <6666alvaro666@gmail.com>
2020-04-16 18:34:15 -0500

    crear los archivos de los componentes

---
+++ src/containers/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Main from '../components/Main';
+import Sidebar from '../components/Sidebar';
+import Info from '../components/Info';
+import About from '../components/About';
+import Social from '../components/Social';
+import Education from '../components/Education';
+import Experince from '../components/Experiences';
+import Certificate from '../components/Certificate';
+import Skills from '../Skills';
+
+const App = ()=>{
+    return(
+        <Main>
+            <Sidebar>
+                <About></About>
+                <Social></Social>
+            </Sidebar>
+            <Info>
+                <Education></Education>
+                <Experince></Experince>
+                <Certificate></Certificate>
+                <Skills></Skills>
+            </Info>
+        </Main>
+    );
+}
+
+export default App;
