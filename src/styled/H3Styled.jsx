0d2afe5388b3e9d099146b0e0172d3559bc77426
Alvaro <6666alvaro666@gmail.com>
2020-04-17 20:14:55 -0500

    creacion de componentes custom

---
+++ src/styled/H3Styled.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledH3 = styled.h3`
+    color: #212121;
+    font-family: 'Roboto', 'sans-serif';
+    font-weight: 400;
+    margin: 0.5em 0 0 0;
+`;
+
+const H3Sthyled =({ children }) => <StyledH3>{children}</StyledH3>
+
+export default H3Sthyled;
