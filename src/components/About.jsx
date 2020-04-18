763595ec43cfafcf6cde66d503eda0238c8ad229
Alvaro <6666alvaro666@gmail.com>
2020-04-17 19:22:26 -0500

    estilizando  el componente about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,36 +6,79 @@ const AboutStyle = styled.div`
     text-align:center;
 `;
 
+const AboutAvatar= styled.div`
+    padding: 2em 0 0 0;
+`;
+
+const AboutImg = styled.img`
+    width:160px;
+    height:160px;
+    border-radius:100%;
+    border: 2px solid #E91E63;
+    margin : 0 auto ;
+    display: block;
+    box-shadow: 0 0 10px rgba(0,0,0,0.6)
+`;
+
+const AboutName = styled.div`
+    text-align:center;
+`;
+const AboutH2 =styled.h2`
+    font-family: 'Roboto', 'sans-serif';
+    font-weight: 400;
+    letter-spacing: 1.2px;
+    margin: 0.5em 0 0 0;
+    color #C2185B;
+`;
+
+const AboutProfesion =styled.p`
+    margin 0.2em 0 1em 0;
+    letter-spacing 2.6px;
+    font-weight:300;
+    color #C2185B
+`;
+
+const AboutBio = styled.p`
+    color:#757575;
+    font-size:1em;
+    font-weight:300;
+`;
+
+const AboutLocation = styled.address`
+color:#757575;
+font-size:1em;
+font-weight:300;
+`;
 
 
 
 const About = ({avatar,name,bio,profession,address,social}) => (
     <AboutStyle>
         <div className="About-container">
-            <div className="About-avatar">
+            <AboutAvatar>
                 <figure>
-                    <img width="200px" src={avatar} alt="About-Avatar"/>
+                    <AboutImg  src={avatar} alt="About-Avatar"/>
                 </figure>
-            </div>
-            <div className="About-name">
-                <h2>
+            </AboutAvatar>
+            <AboutName>
+                <AboutH2>
                     {name}
-                </h2>
-            </div>
+                </AboutH2>
+            </AboutName >
             <div className="About-profession">
-                <p>
+                <AboutProfesion>
                     {profession}
-                </p>
+                </AboutProfesion>
             </div>
             <div className="About-description">
-                <p>
+                <AboutBio>
                    {bio}
-                </p>
+                </AboutBio>
             </div>
             <div className="location">
-                <address>
+                <AboutLocation>
                     {address}
-                </address>
+                </AboutLocation>
             </div>
             <div className="About-social">
                 <Social 
