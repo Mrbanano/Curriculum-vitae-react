1056f8e53c2c52c83ce5f4975772636c9d2ac42e
Alvaro <6666alvaro666@gmail.com>
2020-04-17 22:48:29 -0500

    agregar margin buttom

--- src/styled/H3Styled.jsx
+++ src/styled/H3Styled.jsx
@@ -6,7 +6,7 @@ const StyledH3 = styled.h3`
     color: #212121;
     font-family: 'Roboto', 'sans-serif';
     font-weight: 400;
-    margin: 0.5em 0 0 0;
+    margin: 0.5em 0 0.7em 0;
 `;
 
 const H3Sthyled =({ children }) => <StyledH3>{children}</StyledH3>
