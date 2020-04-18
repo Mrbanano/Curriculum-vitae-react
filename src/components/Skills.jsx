c11d51d0bb39c1bc9fad8acc992093e761717689
Alvaro <6666alvaro666@gmail.com>
2020-04-17 23:16:45 -0500

    agregar la animacion de los skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,79 +1,140 @@
 import React from 'react';
+import styled,{keyframes} from 'styled-components';
+import H3Styled from '../styled/H3Styled';
+import H2Sthyled from '../styled/H2Styled';
+
+
+const SkillsContainer = styled.div`
+    margin:1em 0 2em 0;
+    display:grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap:2em;
+    grid-row-gap: 0.5em;
+`;
+
+const SkillsH5 = styled.h5`
+    margin: 0.5em 0;
+`;
+
+const SkillsLine = styled.div`
+    background-color : #F8BBD0;
+    height: 8px;
+    position: relative;
+    border-radius:50px;
+`;
+const move =keyframes`
+    0%{background-position: 0 0;}
+    100%{background-position: 50px 50px;}
+`;
+
+const SkillsSpan = styled.span`
+    display:block;
+    height:100%;
+    width: ${props => props.width};
+    border-radius: 8px ;
+    background-color: #C2185b;
+    overflow:hidden;
+    position:relative;
+    box-shadow: inset 0 2px 9px rgba(255,255,255,0.3),
+                inset 0 -2px 6px rgba(0,0,0,0.4);
+    &:after{
+        content:"";
+        background-image: linear-gradient(-45deg, rgba(255,255,255,0.2) 25%, transparent 25%, transparent 50%, rgba(255,255,255,0.2) 50%, rgba(255,255,255,0.2) 75%, transparent 75%, transparent);
+        position:absolute;
+        top: 0;
+        left:0;
+        bottom:0;
+        right:0;
+        z-index:1;
+        background-size:50px 50px ;
+        animation: ${move} 4s linear infinite;
+        overflow:hidden;
+        
+    }  
+`;
+
+
 
 const Skills = props => (
     <div className="Skills">
-        <div className="Skills-container">
+        <H2Sthyled name="Skills"></H2Sthyled>
+        <SkillsContainer>
             <div className="Skills-FrontEnd">
+                <H3Styled> FrontEnd </H3Styled>
                 {props.data.skillsFrontEnd.map((front, index) => (
                     <div className="Skills-item" key={`Front-${index}`}>
-                        <h5>
+                        <SkillsH5>
                             {front.name}
-                        </h5>
-                        <div className="Skills-line">
-                            <span>
-                                {front.percentage}
-                            </span>
-                        </div>
+                        </SkillsH5>
+                        <SkillsLine>
+                            <SkillsSpan
+                                width={front.percentage}>
+                            </SkillsSpan>
+                        </SkillsLine>
                     </div>
                 ))}
             </div>
             <div className="Skills-BackEnd">
+            <H3Styled> BackEnd </H3Styled>
                 {props.data.skillsBackEnd.map((Back, index) => (
                     <div className="Skills-item" key={`Back-${index}`}>
-                        <h5>
+                        <SkillsH5>
                             {Back.name}
-                        </h5>
-                        <div className="Skills-line">
-                            <span>
-                                {Back.percentage}
-                            </span>
-                        </div>
+                        </SkillsH5>
+                        <SkillsLine>
+                            <SkillsSpan 
+                                width={Back.percentage}>   
+                            </SkillsSpan>
+                        </SkillsLine>
                     </div>
                 ))}
             </div>
             <div className="Skills-Desing">
+            <H3Styled> Desing </H3Styled>
                 {props.data.skillsDesing.map((DS, index) => (
                     <div className="Skills-item" key={`Desing-${index}`}>
-                        <h5>
+                        <SkillsH5>
                             {DS.name}
-                        </h5>
-                        <div className="Skills-line">
-                            <span>
-                                {DS.percentage}
-                            </span>
-                        </div>
+                        </SkillsH5>
+                        <SkillsLine>
+                            <SkillsSpan
+                                width={DS.percentage}>
+                            </SkillsSpan>
+                        </SkillsLine>
                     </div>
                 ))}
             </div>
             <div className="Skills-Hardware">
+            <H3Styled> Hardware </H3Styled>
                 {props.data.skillsHarware.map((Hw, index) => (
                     <div className="Skills-item" key={`HW-${index}`}>
-                        <h5>
+                        <SkillsH5>
                             {Hw.name}
-                        </h5>
-                        <div className="Skills-line">
-                            <span>
-                                {Hw.percentage}
-                            </span>
-                        </div>
+                        </SkillsH5>
+                        <SkillsLine>
+                            <SkillsSpan
+                                width={Hw.percentage}>
+                            </SkillsSpan>
+                        </SkillsLine>
                     </div>
                 ))}
             </div>
             <div className="Skills-Other">
+            <H3Styled> Team Work </H3Styled>
                 {props.data.skillsOther.map((Ot, index) => (
                     <div className="Skills-item" key={`Other-${index}`}>
-                        <h5>
+                        <SkillsH5>
                             {Ot.name}
-                        </h5>
-                        <div className="Skills-line">
-                            <span>
-                                {Ot.percentage}
-                            </span>
-                        </div>
+                        </SkillsH5>
+                        <SkillsLine>
+                            <SkillsSpan
+                                width={Ot.percentage}>   
+                            </SkillsSpan>
+                        </SkillsLine>
                     </div>
                 ))}
             </div>
-        </div>
+        </SkillsContainer>
     </div>
 );
 
