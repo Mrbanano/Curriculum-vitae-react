d6d29d57855aee69805b7e27b3c83e6b587a24fe
Alvaro <6666alvaro666@gmail.com>
2020-04-17 20:22:01 -0500

    estilizando el componete social

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const socialStyle = styled.div`
+   margin: 0 auto;
+   display:block;
+`;
+const SocialUl = styled.ul`
+    list-style-type:none;
+    margin:0;
+    padding:0;
+`;
+
+const Socialli = styled.li`
+    margin:  0 1em 0 0;
+    display: inline;
+`;
+
+const SocialAnchor = styled.a`
+    color:#212121;
+    text-decoration:none;
+    font-size:1.2em;
+`;
+
 
 const Social = props =>(
-    <div className="Social">
+    <socialStyle>
         {props.social &&
-            <ul>
+            <SocialUl>
                 {props.social.map((item,index)=>(
-                    <li   key={`Social-${index}`} >
-                        <a  target="_blank" href={item.url}>{item.name}</a>
-                    </li>
+                    <Socialli   key={`Social-${index}`} >
+                        <SocialAnchor  target="_blank" href={item.url}>{item.name}</SocialAnchor>
+                    </Socialli>
                 ))}
-            </ul>
+            </SocialUl>
         }
-    </div>
+    </socialStyle>
 );
 
 export default Social;
